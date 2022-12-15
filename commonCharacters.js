53da18e111a3141ad1a227047ed8c463d0efcf55
nagga91 <mnagga@gamil.com>
2022-12-15 11:28:54 +0100

    second commit

--- commonCharacters.js
+++ commonCharacters.js
@@ -14,15 +14,10 @@
 
  var commonCharacters = function(string1, string2) {
    var ch=''
-   for(var i=0;i<string2.length;i++){
-       if(string1.includes(string2[i])){
-           ch+=string2[i]}
+   for(var i=0;i<string1.length;i++){
+       if((string2.includes(string1[i]))&&(!ch.includes(string1[i]))){
+           ch+=string1[i]}
    }
-   while(i<ch.length){
-      if(ch.includes(ch[i])){
-          ch.slice(i,1)}
-      i++
-  }
    return ch
     }
     
